1bf89fb23514cbd3628bc0058daaed11c7560b93
shuaiyu2019211001001301@163.com <sy12318@163.com>
2022-03-15 21:55:05 +0800

    添加项目文件。

---
+++ 1301SHUAIYUERSWindowsFormsApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using _1301SHUAIYUERSWindowsFormsAp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1301SHUAIYUERSWindowsFormsApp
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new EmployeeRecordsForm());
+        }
+    }
+}
